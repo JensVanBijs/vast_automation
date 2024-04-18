9b4382c1355a4d4ea77fc8fb17948d03f034ab51
KingCoenta <s3340287@vuw.leidenuniv.nl>
2024-04-18 16:56:39 +0200

    Create connection with CyUsb device

--- vast_test/test.py
+++ vast_test/test.py
@@ -1,5 +1,19 @@
-import ctypes
+import clr
+clr.AddReferenceToFileAndPath('C:/Program Files (x86)/Union Biometrica/VAST/USB2Comm.dll')
+clr.AddReferenceToFileAndPath('C:/Program Files (x86)/Union Biometrica/VAST/VAST.exe')
+from USB2Comm import UsbComm
+from VAST import AccessConfig, LedCtrl
+from System import UInt16
 
-# Connect with the Usb2Comm.dll
-# Connect with the vast through dll
-# turn on LED through a function
+u = UsbComm()
+acfg = AccessConfig(False)
+usb = UsbComm(u.GetHexFile(
+    clr.Convert(acfg.CypressPIDbeforeRenum, UInt16)), 
+    clr.Convert(acfg.CypressPIDbeforeRenum, UInt16), 
+    clr.Convert(acfg.CypressPIDafterRenum, UInt16)
+    )
+led = LedCtrl()
+led.UsbCom = usb
+
+print(led.LedState)
+
